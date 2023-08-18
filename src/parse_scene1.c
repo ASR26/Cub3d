3bb7fbb94d8fe8d8d0fd76765e48fa2b72e9e9b9
ASR26 <andresolano1026@gmail.com>
2023-08-18 08:51:48 +0200

    added ft_checkdraw function

--- src/parse_scene1.c
+++ src/parse_scene1.c
@@ -46,3 +46,9 @@ t_draw	ft_setdraw(t_cub_info *cub, mlx_t *mlx)
 	draw.e_wall = ft_png_to_img(cub->e_wall, mlx);
 	return (draw);
 }
+int	ft_checkdraw(t_draw draw)
+{
+	if (!draw.n_wall || !draw.s_wall || !draw.e_wall || !draw.w_wall)
+		return (-1);
+	return (0);
+}
