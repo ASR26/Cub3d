0b7957c100109cabe96ccedb5564c00fcf5d5536
ASR26 <andresolano1026@gmail.com>
2023-08-18 09:15:34 +0200

    image opening functions fixed, textures.c created

--- inc/andres.h
+++ inc/andres.h
@@ -13,4 +13,12 @@
 #ifndef ANDRES_H
 # define ANDRES_H
 
+typedef struct s_draw
+{
+	mlx_texture_t   *n_wall;
+	mlx_texture_t	*s_wall;
+	mlx_texture_t	*w_wall;
+	mlx_texture_t	*e_wall;
+}			t_draw;
+
 #endif
